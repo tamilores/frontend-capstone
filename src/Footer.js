934f805ed7e370e85008f5b2b19b97265ac46b6d
tamilores <tamilorsojinrin@gmail.com>
2025-11-14 00:28:20 +0100

    Created visual website

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div><img src="Footerlogo.png" alt="Footer Logo"/></div>
         <div>
             <ul>
-                <li>Doormat Navigation</li>
+                <li><b>Doormat Navigation</b></li>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#menu">Menu</a></li>
@@ -16,7 +16,7 @@ function Footer() {
         </div>
         <div>
             <ul>
-                <li>Contacts</li>
+                <li><b>Contacts</b></li>
                 <li>Address</li>
                 <li>Phone Number</li>
                 <li>Email</li>
@@ -24,7 +24,7 @@ function Footer() {
         </div>
         <div>
             <ul>
-                <li>Social Media Links</li>
+                <li><b>Social Media Links</b></li>
                 <li><a href="#instagram">Instagram</a></li>
                 <li><a href="#twitter">Twitter</a></li>
                 <li><a href="#facebook">Facebook</a></li>
